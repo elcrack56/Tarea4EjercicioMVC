83b02dc8747db3cee480b3f11fad54b678b338a7
elcrack56 <darwinsteven3@gmail.com>
2025-10-21 21:50:52 -0500

    file modification

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.database import mongo
 from bson.objectid import ObjectId
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="static", template_folder="templates")
     app.config.from_object(Config)
 
     mongo.init_app(app)
